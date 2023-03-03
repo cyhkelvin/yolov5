0729a0f589a8987b11cb9c793425cd9ef572960e
Chen Yi-Hsuan <cyhkelvin0530@gmail.com>
2023-03-03 10:27:35 +0800

    Update general.py
    Signed-off-by: Chen Yi-Hsuan <cyhkelvin0530@gmail.com>

--- utils/general.py
+++ utils/general.py
@@ -29,7 +29,7 @@ from typing import Optional
 from zipfile import ZipFile, is_zipfile
 
 import cv2
-import IPython
+# import IPython
 import numpy as np
 import pandas as pd
 import pkg_resources as pkg
@@ -79,6 +79,7 @@ def is_colab():
 
 def is_notebook():
     # Is environment a Jupyter notebook? Verified on Colab, Jupyterlab, Kaggle, Paperspace
+    import IPython
     ipython_type = str(type(IPython.get_ipython()))
     return 'colab' in ipython_type or 'zmqshell' in ipython_type
 
