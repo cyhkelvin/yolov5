1f92df0b2cbedd0be929b3c4d72054d286931a9a
Chen Yi-Hsuan <cyhkelvin0530@gmail.com>
2023-03-09 15:47:46 +0800

    Update detect_server_simple.py
    Signed-off-by: Chen Yi-Hsuan <cyhkelvin0530@gmail.com>

--- detect_server_simple.py
+++ detect_server_simple.py
@@ -32,7 +32,7 @@ import argparse
 import os
 import platform
 import sys
-from pathlib import Path
+from pathlib import Path, PosixPath
 import json
 import torch
 
@@ -70,9 +70,18 @@ class Detector():
         self.weights = weights
         self.model = Detector.load_model(weights, device, dnn, data, half)
 
+    def update_model(self, weights):
+        self.weights = Path(weights[0])
+        self.model = Detector.load_model(weights)
+        return str(self.weights)
+
     @staticmethod
     @smart_inference_mode()
-    def load_model(weights, device, dnn, data, fp16):
+    def load_model(weights, device='', dnn=False, data='data/coco128.yaml', fp16=False):
+        device = select_device(device)
+        if isinstance(data, str):
+            data = Path(data)
+        print(type(weights), weights)
         return DetectMultiBackend(weights=weights, device=device, dnn=dnn, data=data, fp16=fp16)
 
     @smart_inference_mode()
@@ -193,7 +202,8 @@ def delegate(method, args):
         return args
     elif method == 'load_model':
         if detector:
-            detector.model = Detector.load_model(**args)
+            print(args)
+            return detector.update_model(**args)
         else:
             raise RuntimeError("an server error: detector not found")
     elif method == 'detect':
